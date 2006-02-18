7d36953b14d976b0b2fa8fca5a212ffe2ab7f7ca
Egil Moeller <redhog@thalari.redhog.org>
2006-02-18 15:50:47 +0000

    Bah, forgot to add some files...
    git-archimport-id: redhog@thalari.redhog.org/Grimoire--main--0.2--patch-9

---
+++ Documentation/Scripts/Config.d/parameters/clients/_settings/__init__.py
@@ -0,0 +1,21 @@
+## General client configuration ##
+
+# Default tree to connect to
+# set(['tree'], '_')
+# set(['tree'], '_.trees.local.client')
+# set(['tree'], '_.trees.local.login("the LDAP tree on example.com", _.trees.remote.dirt.example\.com().trees.local.ldap)')
+# This allows seamless integration between the webware UI client and
+# some website that uses LDAP for authentication using HTTP AUTH. You
+# must enable passing of the HTTP_AUTHORIZATIOn variable to the
+# webware script in the webserver for this to work, however.
+# set(['tree'], '_.trees.remote.dirt.example\.com().trees.local.ldap(_.directory.get.parameters(["clients", "html", "auth", "username"]), _.directory.get.parameters(["clients", "html", "auth", "password"]))')
+
+# Commands to perform at startup
+# set(['initcommands'], ['_.trees.introspection.mount(['otherserver'], _.trees.remote.dirt.otherserver())'])
+
+# import Grimoire.Types.Ability
+# set(['hide'],
+#     Grimoire.Types.Ability.List([(Grimoire.Types.Ability.Ignore, ['directory']),
+#                                  (Grimoire.Types.Ability.Ignore, ['trees']),
+#                                  (Grimoire.Types.Ability.Ignore, ['introspection']),
+#                                  (Grimoire.Types.Ability.Allow, [])]))
