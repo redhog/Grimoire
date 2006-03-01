533f81cd6b6a7ba5054ebdd74f9d14c92f19c125
Egil Moeller <redhog@thalari.redhog.org>
2006-03-01 10:37:58 +0000

    Made about an object
    git-archimport-id: redhog@thalari.redhog.org/Grimoire--main--0.2--patch-24

--- root/clients/base/_about/about.py
+++ root/clients/base/_about/about.py
@@ -2,6 +2,8 @@ import Grimoire.Performer, Grimoire.Types
 
 class Performer(Grimoire.Performer.Base):
     class about(Grimoire.Performer.Method):
+        __related_group__ = ['about']
+        
         def _call(self):
             return Grimoire.Types.AnnotatedValue(Grimoire.Types.Reducible(Grimoire.About.abouts(), '\n'),
                                                  'About Grimoire')
