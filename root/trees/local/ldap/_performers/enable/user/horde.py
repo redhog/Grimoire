56a5a5fbe720e08fd5a73ca720c6bdcb84d59253
FreeGroup developement server <root@crocodile.freecode.no>
2007-06-19 10:39:52 +0000

    Enabling horde for an account now creates a horde identity
    git-archimport-id: redhog@thalari.redhog.org/Grimoire--main--0.2--patch-173

--- root/trees/local/ldap/_performers/enable/user/horde.py
+++ root/trees/local/ldap/_performers/enable/user/horde.py
@@ -1,4 +1,4 @@
-import Grimoire.Types, Grimoire.Performer, Grimoire.Utils, string, ldap
+import Grimoire.Types, Grimoire.Performer, Grimoire.Utils, string, ldap, base64
 
 A = Grimoire.Types.AnnotatedValue
 Ps = Grimoire.Types.ParamsType.derive
@@ -18,18 +18,55 @@ class Performer(Grimoire.Performer.Base):
             args = {'base': baseDn,
                     'scope': ldap.SCOPE_BASE,
                     'filterstr': 'uid=*',
-                    'attrlist': ['objectClass']}
+                    'attrlist': ['objectClass', 'hordePrefs', 'mail', 'cn']}
             res = conn.result(conn.search(**args))
+
+            mod = []
+
+            modObjectClasses = False
             objectClasses = list(res[1][0][1]['objectClass'])
-            mod = False
             if 'hordePerson' not in objectClasses:
                 objectClasses.append('hordePerson')
-                mod = True
+                modObjectClasses = True
             if 'top' not in objectClasses:
                 objectClasses.append('top')
-                mod = True
+                modObjectClasses = True
+            if modObjectClasses:
+                mod.append((ldap.MOD_REPLACE, 'objectClass', objectClasses))
+                
+            if 'hordePrefs' not in res[1][0][1]:
+                # Note on hordePrefs:
+                # Each hordePrefs attribute consists of a pair of a
+                # keyword and a value separated by a colon. The value
+                # is base64-encoded. For some preferences, the value
+                # is further encoded in a simple hollerith-based
+                # format. Each entry consists of a type, an optional
+                # length and a value separated by colons.
+                # type    description    format for value
+                # i       integer        Length is missing. Value is
+                #                        the decimal representation
+                #                        of the value.
+                # s       string         String is utf-8-encoded and
+                #                        surrounded by ". Length is
+                #                        number of bytes excluding the
+                #                        ".
+                # a       array          length is some magic number,
+                #                        value is a set of entries
+                #                        separated by cemi-colon, and
+                #                        surrounded by curly braces. 
+                
+                prefs = ['identities:' + base64.encodestring(
+                    """a:1:{i:0;a:4:{s:2:"id";s:%(fullname_len)s:"%(fullname)s";s:8:"fullname";s:%(fullname_len)s:"%(fullname)s";s:9:"from_addr";s:%(email_len)s:"%(email)s";s:16:"default_identity";s:1:"0";}}"""
+                    % {'fullname': Grimoire.Utils.encode(res[1][0][1]['cn'][0], 'utf-8'),
+                       'fullname_len': str(len(Grimoire.Utils.encode(res[1][0][1]['cn'][0], 'utf-8'))),
+                       'email': Grimoire.Utils.encode(res[1][0][1]['mail'][0], 'utf-8'),
+                       'email_len': str(len(Grimoire.Utils.encode(res[1][0][1]['mail'][0], 'utf-8')))
+                       }),
+                         'default_identity:' + base64.encodestring('0')]
+                mod.append((ldap.MOD_REPLACE, 'hordePrefs', prefs))
+                
             if mod:
-                conn.modify_s(baseDn, [(ldap.MOD_REPLACE, 'objectClass', objectClasses)])
+                conn.modify_s(baseDn, mod)
             else:
                 return A(None, "Horde web interface already enabled for account")
             return A(None, "Horde web interface enabled for account")
